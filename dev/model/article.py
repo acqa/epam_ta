c0209a651de24f153f2489ee1bb2d1040955007a
acqa <47945113+acqa@users.noreply.github.com>
2019-05-26 18:21:39 +0300

    Параметризация тестов

--- dev/model/article.py
+++ dev/model/article.py
@@ -4,3 +4,9 @@ class Article:
     def __init__(self, title, note):
         self.title = title
         self.note = note
+
+    def __repr__(self):
+    	'''
+    	Используется в test_aritle для вывода значений в параметризации теста
+    	'''
+    	return "%s:%s" % (self.title, self.note)
--- dev/test/test_article.py
+++ dev/test/test_article.py
@@ -1,5 +1,16 @@
 import time
 from model.article import Article
+import pytest
+import random
+import string
+
+
+def random_string(prefix, maxlen):
+	'''
+	Генератор случайных строк случайно длины, но не больше заданной
+	'''
+	symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+	return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
 # Test data
@@ -8,12 +19,38 @@ testNote = 'Тестовая запись ' + str(time.localtime())
 testdata = {"testTitle":testTitle, "testNote":testNote}
 
 
+testdata2 = [
+	Article(title = 'First title', note = 'First note'),
+	Article(title = 'Empty note', note = ''),
+	Article(title = '', note = 'Empty title'),
+	Article(title = random_string("Tilte ", 10), note = random_string("Note ", 20))
+]
+
+testdata3 = [
+	Article(title = 'First title', note = 'First note'),
+	Article(title = 'Empty note', note = ''),
+	Article(title = '', note = 'Empty title')
+	] + [
+	Article(title = random_string("Tilte ", 10), note = random_string("Note ", 20)) for i in range(3)
+	]
+	
+testdata4 = [
+	Article(title = title, note = note)
+	for title in ["", random_string("Tilte ", 10)]
+	for note in [random_string("Note ", 20)]
+]
 
 
 
 
+@pytest.mark.parametrize("article", testdata3, ids = [repr(x) for x in testdata3])
+def test_create_article_param(app, article):
+	pass
+	##app.article.create(articleTitle=testdata['testTitle'], articleText=testdata['testNote'])
+	#app.article.create(Article(title=testdata['testTitle'], note=testdata['testNote']))
+	app.article.create(article)
 
-#@pytest.mark.parametrize("article", testdata)
+
 def test_create_article(app):
 	#app.article.create(articleTitle=testdata['testTitle'], articleText=testdata['testNote'])
 	app.article.create(Article(title=testdata['testTitle'], note=testdata['testNote']))
