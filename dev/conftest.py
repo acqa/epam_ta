78f9d77254909136c325ecaa663a582175f7bd75
acqa <reg@ac11.ru>
2019-05-31 16:05:36 +0300

    Update conftest.py

--- dev/conftest.py
+++ dev/conftest.py
@@ -9,13 +9,14 @@ import importlib
 fixture = None
 target = None
 
+
 @pytest.fixture()
 def app(request):
-	'''
-	Интеллектуальная фикстура, с проверкой валидности. 
+	"""	Интеллектуальная фикстура, с проверкой валидности
 	Создает одну сессию для всех тестов без scope = 'session'
 	Переработана
-	'''
+
+	"""
 	global fixture
 	global target
 	browser = request.config.getoption('--browser') # Задаем название браузера в опции командной строки
@@ -33,9 +34,8 @@ def app(request):
 
 @pytest.fixture(scope = 'session', autouse = True)
 def stop(request):
-	'''
-	Завершаем браузер в конце тестов.
-	'''
+	"""Завершаем браузер в конце тестов.
+	"""
 	def fin():
 		#fixture.session.logout()
 		fixture.destroy()
@@ -43,18 +43,16 @@ def stop(request):
 	return fixture
 
 def pytest_addoption(parser):
-	'''
-	Функция pytest-а для добавления опций в командной строке
-	'''
+	"""Функция pytest-а для добавления опций в командной строке
+	"""
 	parser.addoption('--browser', action = 'store', default = 'chrome') # выбор браузера
 	parser.addoption('--target', action = 'store', default = 'config/target.json') # загрузка параметров из конфиг. файла
 
 
 def pytest_generate_tests(metafunc):
-	'''
-	Функция производит загрузку тестовых данных из файла articles.py в пакете data (задается в тесте)
+	"""	Функция производит загрузку тестовых данных из файла articles.py в пакете data (задается в тесте)
 	или из файла articles.json в из того же пакета.
-	'''
+	"""
 	for fixture in metafunc.fixturenames:
 		if fixture.startswith("data_"):
 			testdata = load_from_module(fixture[5:])
@@ -65,9 +63,13 @@ def pytest_generate_tests(metafunc):
 
 
 def load_from_module(module):
+	"""Производим загрузку тестовых данных из модуля
+	"""
 	return importlib.import_module("data.%s" % module).testdata
 
 def load_from_json(json_file):
+	"""Производим загрузку тестовых данных из json-файла
+	"""
 	with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % json_file)) as f:
 		return jsonpickle.decode((f.read()))
 
